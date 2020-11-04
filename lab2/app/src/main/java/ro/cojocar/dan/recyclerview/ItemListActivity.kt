84e4f2fe717e7a2256a88799f8f8153ce1f18d96
Natan <natangabriel99@yahoo.com>
2020-11-04 18:53:25 +0200

    first commit

---
+++ lab2/app/src/main/java/ro/cojocar/dan/recyclerview/ItemListActivity.kt
@@ -0,0 +1,91 @@
+package ro.cojocar.dan.recyclerview
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_item_detail.*
+import kotlinx.android.synthetic.main.activity_item_list.*
+import kotlinx.android.synthetic.main.activity_item_list.fab
+import kotlinx.android.synthetic.main.item_list.*
+import kotlinx.android.synthetic.main.item_list_content.view.*
+import ro.cojocar.dan.recyclerview.dummy.DummyContent
+
+class ItemListActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_item_list)
+
+    setSupportActionBar(toolbar)
+    toolbar.title = title
+
+    fab.setOnClickListener { view ->
+      val intent = Intent(view.context, ItemAddActivity::class.java).apply {}
+      view.context.startActivity(intent)
+    }
+
+    setupRecyclerView(item_list)
+
+
+
+  }
+
+  private fun setupRecyclerView(recyclerView: RecyclerView) {
+    recyclerView.adapter = SimpleItemRecyclerViewAdapter(DummyContent.ITEMS)
+  }
+
+  class SimpleItemRecyclerViewAdapter(
+      private val values: List<DummyContent.Aircraft>
+  ) : RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder>() {
+
+    private val onClickListener: View.OnClickListener
+
+    init {
+      onClickListener = View.OnClickListener { v ->
+
+        val item = v.tag as DummyContent.Aircraft
+        val intent = Intent(v.context, ItemDetailActivity::class.java).apply {
+          putExtra(ItemDetailFragment.ARG_ITEM_ID, item.tailNumber)
+        }
+        Log.i("tagitemlistact",";;;"+item+";;;"+intent+";;;"+ItemDetailFragment.ARG_ITEM_ID+";;;"+item.tailNumber)
+        v.context.startActivity(intent)
+      }
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+      val view = LayoutInflater.from(parent.context)
+          .inflate(R.layout.item_list_content, parent, false)
+      return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+      val item = values[position]
+      holder.idView.text = item.flightCode
+      holder.terminal.text = item.terminal
+      holder.gate.text = item.gate
+
+      with(holder.itemView) {
+        tag = item
+        setOnClickListener(onClickListener)
+      }
+    }
+
+    override fun getItemCount() = values.size
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+      val idView: TextView = view.id_text
+      val terminal: TextView = view.terminal
+      val gate: TextView = view.gate
+    }
+  }
+}
---
+++ lab2/app/src/main/java/ro/cojocar/dan/recyclerview/dummy/DummyContent.kt
@@ -0,0 +1,57 @@
+package ro.cojocar.dan.recyclerview.dummy
+
+import java.util.*
+
+object DummyContent {
+
+  val ITEMS: MutableList<Aircraft> = ArrayList()
+
+  val ITEM_MAP: MutableMap<String, Aircraft> = HashMap()
+
+  private const val COUNT = 25
+
+  init {
+
+    addItem(createAircraftItem("G-AAIN","Airbus A320","British Airways","BA0751","Terminal 4","D04"))
+    addItem(createAircraftItem("G-AWIN","Airbus A380","Air France","AF0051","Terminal 4","D05"))
+    addItem(createAircraftItem("G-TYIN","Airbus A330","British Airways","BA0701","Terminal 2","B04"))
+    addItem(createAircraftItem("G-TRIN","Airbus A350","Wizz Air","W60251","Terminal 5","E04"))
+    addItem(createAircraftItem("G-AAEE","Airbus A310","American Airlines","AA0791","Terminal 3","C04"))
+    addItem(createAircraftItem("A-AHGF","Airbus A340","KLM","KL0051","Terminal 4","D06"))
+    // Add some sample items.
+//    for (i in 1..COUNT) {
+//      addItem(createAircraftItem())
+//    }
+  }
+
+  private fun addItem(item: Aircraft) {
+    ITEMS.add(item)
+    ITEM_MAP[item.tailNumber] = item
+  }
+
+  private fun createAircraftItem(tailNumber: String,aircraftType:String,airline:String,flightCode:String,terminal:String,gate:String): Aircraft {
+    return Aircraft(tailNumber,aircraftType,airline,flightCode,terminal,gate)
+  }
+
+
+  private fun createDummyItem(position: Int): DummyItem {
+    return DummyItem(position.toString(), "Item $position", makeDetails(position))
+  }
+
+  private fun makeDetails(position: Int): String {
+    val builder = StringBuilder()
+    builder.append("Details about Item: ").append(position)
+    for (i in 0 until position) {
+      builder.append("\nMore details information here.")
+    }
+    return builder.toString()
+  }
+
+  data class Aircraft(val tailNumber: String,val aircraftType:String,val airline:String,val flightCode:String,val terminal:String,val gate:String) {
+    override fun toString(): String = flightCode + airline
+  }
+
+  data class DummyItem(val id: String, val content: String, val details: String) {
+    override fun toString(): String = content
+  }
+}
