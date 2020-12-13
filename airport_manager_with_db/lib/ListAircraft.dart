8db5695e3ee99b9085bf339676971a3c9f1a0ca7
Natan <natangabriel99@yahoo.com>
2020-12-13 19:45:32 +0200

    lab finished 2.0

--- airport_manager_with_db/lib/ListAircraft.dart
+++ airport_manager_with_db/lib/ListAircraft.dart
@@ -1,3 +1,4 @@
+import 'package:airport_manager/size_config/size_config.dart';
 import 'package:flutter/material.dart';
 import 'Aircraft.dart';
 import 'AddPage.dart';
@@ -40,7 +41,7 @@ class _ListAircraftState extends State<ListAircraft> {
         title: Text('Airport Manager'),
         
       ),
-      body: _buildAircrafts(),
+      body: _buildAircrafts1(),//createTable(),//_buildAircrafts(),//createTable(),  <-if you want the version from previous lab
       floatingActionButton: FloatingActionButton(
         onPressed: _add,
         tooltip: 'Increment',
@@ -118,9 +119,117 @@ class _ListAircraftState extends State<ListAircraft> {
         );
       },
     );
-}
+}  
 
-    Widget _buildAircrafts() {
+    Widget createTable() {
+      List<DataColumn> _columns = [];
+      List<DataRow> _rows = [];
+      _columns.add(DataColumn(label:Text("Flight\ncode",textScaleFactor: 1.5)));
+      _columns.add(DataColumn(label:Text("Terminal",textScaleFactor: 1.5)));
+      _columns.add(DataColumn(label:Text("Gate",textScaleFactor: 1.5)));
+      _columns.add(DataColumn(label:Text("",textScaleFactor: 1.5)));
+
+      for (int i = 0; i < _aircrafts.length;++i) {
+        _rows.add(_createRow(_aircrafts[i]));
+      }
+
+      return SingleChildScrollView(
+        padding: const EdgeInsets.only(top:20.0,bottom:80.0),
+        
+        child:DataTable(
+              columnSpacing: 10.0,
+              showCheckboxColumn: false,
+              columns: _columns,
+              rows: _rows
+            ));
+
+    }
+
+    Widget customContainer(String str){
+    return Container(
+      width: SizeConfig.blockSizeHorizontal * 20,
+      child: Text(str,textScaleFactor: 1.5),
+    );
+  }
+
+  DataRow _createRow(Aircraft aircraft) {
+
+    return  DataRow(cells:<DataCell> [
+          DataCell(customContainer(aircraft.flightCode)),
+          DataCell(customContainer(aircraft.terminal)),
+          DataCell(customContainer(aircraft.gate)),
+          DataCell(Container(
+              width: SizeConfig.blockSizeHorizontal * 19,
+              child:FlatButton(
+              
+              onPressed: () { _showMyDialog(aircraft); },
+              child: Text(
+                "Delete",
+              ),
+              color: Colors.red,
+            ))),
+        ],
+        onSelectChanged: (bool selected) {
+                if (selected) {
+                    _detail(aircraft);
+                }
+            }
+        );
+  }
+
+
+  Widget _buildAircrafts1() {
+    return ListView.builder(
+      padding: const EdgeInsets.only(top:16,left:16,right:16,bottom: 80),
+       itemCount: _aircrafts.length*2,
+      // The itemBuilder callback is called once per suggested 
+      // word pairing, and places each suggestion into a ListTile
+      // row. For even rows, the function adds a ListTile row for
+      // the word pairing. For odd rows, the function adds a 
+      // Divider widget to visually separate the entries. Note that
+      // the divider may be difficult to see on smaller devices.
+      itemBuilder: (BuildContext _context, int i) {
+        // Add a one-pixel-high divider widget before each row 
+        // in the ListView.
+        if (i.isOdd) {
+          return Divider();
+        }
+        // The syntax "i ~/ 2" divides i by 2 and returns an 
+        // integer result.
+        final int index = i ~/ 2;
+        return _buildRow1(_aircrafts[index]);
+      }
+    );
+  }
+
+  Widget _buildRow1(Aircraft aircraft) {
+
+    return ListTile(
+      title: Text(
+        "Flight code: "+aircraft.flightCode+"\n",
+        
+        style: _biggerFont,
+      ),
+      isThreeLine: true,
+      subtitle: Text("Terminal: "+aircraft.terminal+"\n"+
+        "Gate: "+aircraft.gate),
+
+      trailing: FlatButton(
+              onPressed: () { _showMyDialog(aircraft); },
+              child: Text(
+                "Delete",
+              ),
+              color: Colors.red,
+            ),
+
+      onTap: () {      
+        _detail(aircraft);
+    },
+
+    );
+  }
+
+  Widget _buildAircrafts() {
     return ListView.builder(
       padding: const EdgeInsets.all(16),
        itemCount: _aircrafts.length*2,
@@ -168,6 +277,7 @@ class _ListAircraftState extends State<ListAircraft> {
 
     );
   }
+
 
 
 }
--- airport_manager_with_db/lib/main.dart
+++ airport_manager_with_db/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:airport_manager/size_config/size_config.dart';
 import 'package:flutter/material.dart';
 import 'ListAircraft.dart';
 
@@ -12,6 +13,90 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.green,
       ),                       
       home: ListAircraft(),
+    );
+  }
+}
+
+
+
+class Testing extends StatefulWidget {
+  Testing({Key key}) : super(key: key);
+
+  @override
+  _MyDataTableState createState() => _MyDataTableState();
+}
+
+class _MyDataTableState extends State<Testing> {
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("DataTable"),
+      ),
+      body: Row(
+        children: <Widget>[
+          //TODO: use Expanded here
+          Expanded(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(0.0),
+              child: DataTable(
+                columnSpacing: 0,
+                columns: [
+                  DataColumn(
+                    label: 
+                    // label: Text('Item Code'),
+                     Container(
+                      width: SizeConfig.blockSizeHorizontal * 50,
+                      child: Text('ItemCodeaaaaaaaaaaaaaaaaaaaaaaaaagggggggg'),
+                    ),
+                  ),
+                  DataColumn(
+                    // label: Text('Stock Item'),
+                    label: Container(
+                      width: SizeConfig.blockSizeHorizontal * 50,
+                      child: Text('Stock Item'),
+                    ),
+                  ),
+                ],
+                rows: [
+                  DataRow(
+                    cells: [
+                      DataCell(
+                        //Text('Yup.  text.'),
+                        Container(
+                          width: SizeConfig.blockSizeHorizontal * 40,
+                        child:Text(
+                          'Yup.  text.'),
+                            //'This is a really long text. It\'s supposed to be long so that I can figure out what in the hell is happening to the ability to have the text wrap in this datacell.  So far, I haven\'t been able to figure it out.'),
+                      )
+                      ),
+                      DataCell(
+                        // Container(
+                        //   width: SizeConfig.blockSizeHorizontal * 40,
+                        // child:Text(
+                        //   'Thissareallylongtext.It\'ssupposedtobelongsothat I can figure out what in the hell is happening to the ability to have the text wrap in this datacell.  So far, I haven\'t been able to figure it out.'),
+                           customContainer( 'This is a really long text. It\'s supposed to be long so that I can figure out what in the hell is happening to the ability to have the text wrap in this datacell.  So far, I haven\'t been able to figure it out.'),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+
+  Widget customContainer(String str){
+    return Container(
+      width: SizeConfig.blockSizeHorizontal * 30,
+      child: Text(str),
     );
   }
 }
